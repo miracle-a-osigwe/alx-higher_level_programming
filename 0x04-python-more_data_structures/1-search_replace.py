404d36e844137f177d69ed7f72294d2e99e983de
miracle-a-osigwe <miracleosigwe6@gmail.com>
2022-08-31 08:58:53 -0700

    Add a new function

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    loc = my_list.index(search)
-    new_list = my_list.copy()
-    new_list.pop(loc)
-    new_list.insert(loc, replace)
-    return new_list
+    try:
+        loc = my_list.index(search)
+        new_list = my_list.copy()
+        new_list.pop(loc)
+        new_list.insert(loc, replace)
+        return new_list
+    except Exception as e:
+        return None
