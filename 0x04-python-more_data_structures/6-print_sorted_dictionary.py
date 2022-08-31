651b51962ee6ace9787a002c2fcb5368e7816f8e
miracle-a-osigwe <miracleosigwe6@gmail.com>
2022-08-31 09:34:16 -0700

    Add a new function

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,4 +4,4 @@ def print_sorted_dictionary(a_dictionary):
     keys = a_dictionary.keys()
     sorted_keys = sorted(keys)
     for key in sorted_keys:
-        print("{} : {}".format(key, a_dictionary[key]))
+        print("{}: {}".format(key, a_dictionary[key]))
