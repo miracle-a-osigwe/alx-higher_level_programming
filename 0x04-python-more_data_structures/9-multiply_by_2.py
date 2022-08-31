1606437bbace15d404b680e0015cc86b2aad1f4b
miracle-a-osigwe <miracleosigwe6@gmail.com>
2022-08-31 14:05:24 -0700

    Add new function

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary:
-        value = a_dictionary[key]
-        a_dictionary[key] = value * 2
-    return a_dictionary
+    new_dict = a_dictionary.copy()
+    for key in new_dict:
+        value = new_dict[key]
+        new_dict[key] = value * 2
+    return new_dict
