910eccd3103403b94b9e21c1693c460bc7b6770b
miracle-a-osigwe <miracleosigwe6@gmail.com>
2022-09-12 14:50:30 -0700

    Add python exception files

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,8 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            print(my_list[i])
+            print(my_list[i], end="")
             count += 1
         except IndexError as index:
+            print()
             return (count)
