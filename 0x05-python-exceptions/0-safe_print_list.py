c9f2272d526c8986760bff9ef96218ea5e82a393
miracle-a-osigwe <miracleosigwe6@gmail.com>
2022-09-13 09:28:55 -0700

    Update file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             count += 1
     except IndexError as index:
-            pass
+        pass
     finally:
         print()
         return count
